4d5f50b95ee8bdf0f55bd5c07a632a0a1c682d08
Pablo Bianciotto <bianciottopablo@gmail.com>
2018-02-09 19:51:37 -0300

    Fix workers

--- build_worker/canvas_updater.js
+++ build_worker/canvas_updater.js
@@ -61,6 +61,7 @@ var pusher = new Pusher({
   appId: process.env.PUSHER_APP_ID,
   key: process.env.PUSHER_APP_KEY,
   secret: process.env.PUSHER_APP_SECRET,
+  cluster: process.env.PUSHER_APP_CLUSTER,
   encrypted: true
 });
 
